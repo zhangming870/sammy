2531a6549aa46faa6021d0fe80400c66f41f35ff
zhangming870 <zhangming870@hotmail.com>
2015-11-17 16:38:51 -0500

    env setup

---
+++ sammy/sammy/OfficialTutorial/json_store.js
@@ -0,0 +1,2 @@
+﻿/// <reference path="C:\Users\mzhang\Source\Repos\sammy\sammy\sammy\Scripts/jquery-2.1.4.js" />
+/// <reference path="C:\Users\mzhang\Source\Repos\sammy\sammy\sammy\Scripts/sammy-0.7.5.js" />
--- sammy/sammy/Scripts/json_store.js
+++
@@ -1,2 +0,0 @@
-﻿/// <reference path="sammy-0.7.5.js" />
-/// <reference path="jquery-2.1.4.js" />
